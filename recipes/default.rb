ce5d6c56b567d1e9532781193180ae9277436a37
Lance Albertson <lance@osuosl.org>
2017-06-23 09:47:08 -0700

    Initial default recipe
    This pulls in some logic from the various places we're already using docker. The
    upstream cookbook doesn't have a default recipe and is only resource driven, so
    this cookbook will make an assumption that we want a default docker service
    running utilizing dynamic attributes for setting the various parameters for each
    resource. A few other notable features:
    
    - Prefer using upstream vendor repositories instead of distribution repos to
      ensure we're using the latest and greatest.
    - This uses apt pinning and yum-versionlock to ensure we have the exact version
      of docker installed we want. That way we don't upgrade when we don't want to
      accidentally.

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,4 +15,34 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+if node['platform_family'] == 'rhel'
+  include_recipe 'yum-docker'
+  include_recipe 'yum-plugin-versionlock'
 
+  yum_version_lock 'docker-engine' do
+    version node['osl-docker']['package']['version']
+    release '1.el7.centos'
+    notifies :makecache, 'yum_repository[docker-main]', :immediately
+  end
+end
+
+include_recipe 'apt-docker' if node['platform_family'] == 'debian'
+
+apt_preference 'docker-engine' do
+  pin "version #{node['osl-docker']['package']['version']}*"
+  pin_priority '1001'
+  only_if { node['platform_family'] == 'debian' }
+end
+
+docker_installation_package 'default' do
+  node['osl-docker']['package'].each do |key, value|
+    send(key.to_sym, value)
+  end
+end
+
+docker_service 'default' do
+  node['osl-docker']['service'].each do |key, value|
+    send(key.to_sym, value)
+  end
+  action [:create, :start]
+end
