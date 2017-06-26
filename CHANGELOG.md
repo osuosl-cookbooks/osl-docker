dfe19da68ff8d322d8b0272732a67fee3dbdb525
Manatee <github@osuosl.org>
2017-06-26 15:57:09 +0000

    Automatic major-level version bump to v1.0.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ osl-docker CHANGELOG
 This file is used to list changes made in each version of the
 osl-docker cookbook.
 
+1.0.0 (2017-06-26)
+------------------
+- Initial default recipe
+
 0.1.0
 -----
 - Initial release of osl-docker
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ issues_url       'https://github.com/osuosl-cookbooks/osl-docker/issues'
 source_url       'https://github.com/osuosl-cookbooks/osl-docker'
 description      'Installs/Configures osl-docker'
 long_description 'Installs/Configures osl-docker'
-version          '0.1.0'
+version          '1.0.0'
 
 depends          'apt'
 depends          'apt-docker'
