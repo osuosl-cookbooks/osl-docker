c404add5ac260b9d611a62375a5de32b57b4a4d7
Manatee <github@osuosl.org>
2017-08-18 23:18:07 +0000

    Automatic patch-level version bump to v1.0.1 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ osl-docker CHANGELOG
 This file is used to list changes made in each version of the
 osl-docker cookbook.
 
+1.0.1 (2017-08-18)
+------------------
+- Set package_name to docker-engine instead of docker-ce
+
 1.0.0 (2017-06-26)
 ------------------
 - Initial default recipe
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ issues_url       'https://github.com/osuosl-cookbooks/osl-docker/issues'
 source_url       'https://github.com/osuosl-cookbooks/osl-docker'
 description      'Installs/Configures osl-docker'
 long_description 'Installs/Configures osl-docker'
-version          '1.0.0'
+version          '1.0.1'
 
 depends          'apt'
 depends          'apt-docker'
