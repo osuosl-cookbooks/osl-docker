510f4b9399eaa551adff1306401304bfb9ee470e
Lance Albertson <lance@osuosl.org>
2017-09-21 15:41:29 -0700

    Add firewall rules for docker on powerci

--- test/integration/powerci/serverspec/powerci_spec.rb
+++ test/integration/powerci/serverspec/powerci_spec.rb
@@ -18,3 +18,8 @@ end
 describe port(2375) do
   it { should be_listening }
 end
+
+describe iptables do
+  it { should have_rule('-A docker -s 192.168.6.0/24 -p tcp -m tcp --dport 2375 -j ACCEPT') }
+  it { should have_rule('-A docker -s 192.168.6.0/24 -p tcp -m tcp --dport 2376 -j ACCEPT') }
+end
