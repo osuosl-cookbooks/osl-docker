461d1bcfc89efe025c509258e3c33383a6bed542
Lance Albertson <lance@osuosl.org>
2017-11-09 10:14:45 -0800

    Move common docker ServerSpec into a helper
    This simplifies doing basic docker testing with any recipe without having to
    duplicate code.

---
+++ test/integration/helpers/serverspec/spec_helper.rb
@@ -0,0 +1,22 @@
+require 'serverspec'
+set :backend, :exec
+
+shared_examples_for 'docker' do
+  describe service('docker') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe command('docker --version') do
+    case os[:family]
+    when 'redhat'
+      its(:stdout) { should match(/17\.06\.1-ce/) }
+    when 'debian'
+      its(:stdout) { should match(/17\.05\.0-ce/) }
+    end
+  end
+
+  describe command('docker ps') do
+    its(:exit_status) { should eq 0 }
+  end
+end
--- test/integration/powerci/serverspec/powerci_spec.rb
+++ test/integration/powerci/serverspec/powerci_spec.rb
@@ -1,18 +1,7 @@
-require 'serverspec'
+require 'spec_helper'
 
-set :backend, :exec
-
-describe service('docker') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe command('docker --version') do
-  its(:stdout) { should match(/17\.06\.1-ce/) }
-end
-
-describe command('docker ps') do
-  its(:exit_status) { should eq 0 }
+describe 'docker' do
+  it_behaves_like 'docker'
 end
 
 describe port(2375) do
