2c181f31be0e1986737af06df79933fc1d8481ad
Lance Albertson <lance@osuosl.org>
2017-12-07 10:50:26 -0800

    s390x support via tarball installation method
    Docker does not have a yum repo for s390x (yet), so we can use the binary build
    of it instead.

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,4 +9,14 @@ when 'debian'
   default['osl-docker']['package']['package_name'] = 'docker-ce'
   default['osl-docker']['package']['version'] = '17.09.0'
 end
+case node['kernel']['machine']
+when 's390x'
+  default['osl-docker']['tarball']['version'] = '17.09.0'
+  default['osl-docker']['tarball']['checksum'] = '6037fbd5e22d68fde6ddf73a28c10b5c37d916b02b6848a3cb62344ced137365'
+  default['osl-docker']['tarball']['source'] =
+    'https://download.docker.com/linux/static/stable/s390x/docker-' \
+    "#{default['osl-docker']['tarball']['version']}-ce.tgz"
+else
+  default['osl-docker']['tarball'] = {}
+end
 default['osl-docker']['service'] = {}
