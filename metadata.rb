b9795a9cd489731abbbc05170e20a9d7bd17e327
Jason Chen <jasonc@osuosl.org>
2018-07-06 15:30:29 -0700

    Foodcritic fixes

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
 name             'osl-docker'
 maintainer       'Oregon State University'
 maintainer_email 'chef@osuosl.org'
-license          'apachev2'
+license          'Apache-2.0'
+chef_version     '>= 12.18' if respond_to?(:chef_version)
 issues_url       'https://github.com/osuosl-cookbooks/osl-docker/issues'
 source_url       'https://github.com/osuosl-cookbooks/osl-docker'
 description      'Installs/Configures osl-docker'
