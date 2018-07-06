c6f99379b7f02e5f2fcdb0d33d5f8c92ac9e8b3e
Jason Chen <jasonc@osuosl.org>
2018-07-06 11:09:37 -0700

    Switch to cookstyle and apply fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,25 +5,25 @@ ChefSpec::Coverage.start! { add_filter 'osl-docker' }
 
 CENTOS_7 = {
   platform: 'centos',
-  version: '7.2.1511'
+  version: '7.2.1511',
 }.freeze
 
 DEBIAN_8 = {
   platform: 'debian',
-  version: '8.4'
+  version: '8.4',
 }.freeze
 
 ALL_PLATFORMS = [
   CENTOS_7,
-  DEBIAN_8
+  DEBIAN_8,
 ].freeze
 
 CENTOS_PLATFORMS = [
-  CENTOS_7
+  CENTOS_7,
 ].freeze
 
 DEBIAN_PLATFORMS = [
-  DEBIAN_8
+  DEBIAN_8,
 ].freeze
 
 RSpec.configure do |config|
