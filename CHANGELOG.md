91ad7cfb32717b08b67b8c6a9c00ce5fa0c338e2
Manatee <github@osuosl.org>
2021-07-01 20:45:41 +0000

    Automatic minor-level version bump to v4.1.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ osl-docker CHANGELOG
 This file is used to list changes made in each version of the
 osl-docker cookbook.
 
+4.1.0 (2021-07-01)
+------------------
+- Enable live restore globally
+
 4.0.0 (2021-07-01)
 ------------------
 - Install latest docker and remove version pinning
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ chef_version     '>= 16.0'
 issues_url       'https://github.com/osuosl-cookbooks/osl-docker/issues'
 source_url       'https://github.com/osuosl-cookbooks/osl-docker'
 description      'Installs/Configures osl-docker'
-version          '4.0.0'
+version          '4.1.0'
 
 depends          'apt'
 depends          'certificate'
