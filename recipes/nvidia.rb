86afaf1897781627cb8858bb62b8d65df70a0a45
Lance Albertson <lance@osuosl.org>
2022-09-15 16:59:02 -0700

    Switch to using osl-gpu cookbook for nvidia recipe
    Removes all this extra logic to a better cookbook.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- recipes/nvidia.rb
+++ recipes/nvidia.rb
@@ -24,106 +24,31 @@ node.default['osl-docker']['daemon']['runtimes'] = {
   },
 }
 
-include_recipe 'yum-epel'
-include_recipe 'yum-nvidia'
+osl_nvidia_driver 'latest'
 
-build_essential 'nvidia'
+yum_repository 'libnvidia-container' do
+  baseurl 'https://nvidia.github.io/libnvidia-container/stable/centos$releasever/$basearch'
+  gpgkey 'https://nvidia.github.io/libnvidia-container/gpgkey'
+end
 
 include_recipe 'osl-docker'
-include_recipe 'yum-plugin-versionlock'
 
-version_lock = node['osl-docker']['nvidia']['version_lock']
-makecache_file = ::File.join(Chef::Config[:file_cache_path], 'makecache-cuda')
-
-%w(
-  dkms-nvidia
-  kmod-nvidia-latest-dkms
-  nvidia-driver
-  nvidia-driver-cuda
-  nvidia-driver-cuda-libs
-  nvidia-driver-devel
-  nvidia-driver-latest
-  nvidia-driver-latest-cuda
-  nvidia-driver-latest-dkms
-  nvidia-driver-latest-dkms-cuda
-  nvidia-driver-latest-dkms-cuda-libs
-  nvidia-driver-latest-dkms-devel
-  nvidia-driver-latest-dkms-libs
-  nvidia-driver-latest-dkms-NvFBCOpenGL
-  nvidia-driver-latest-dkms-NVML
-  nvidia-driver-libs
-  nvidia-driver-NvFBCOpenGL
-  nvidia-driver-NVML
-  nvidia-kmod
-  nvidia-libXNVCtrl
-  nvidia-libXNVCtrl-devel
-  nvidia-modprobe
-  nvidia-modprobe-latest
-  nvidia-modprobe-latest-dkms
-  nvidia-persistenced
-  nvidia-persistenced-latest
-  nvidia-persistenced-latest-dkms
-  nvidia-settings
-  nvidia-xconfig
-  nvidia-xconfig-latest
-  nvidia-xconfig-latest-dkms
-).each do |p|
-  yum_version_lock p do
-    version version_lock['nvidia-driver']['version']
-    release version_lock['nvidia-driver']['release']
-    epoch 3
-    notifies :touch, "file[#{makecache_file}]", :immediately
-  end
+package 'nvidia-docker2' do
+  notifies :create, 'template[/etc/docker/daemon.json]', :immediately
 end
 
-yum_version_lock 'cuda-drivers' do
-  version version_lock['cuda-drivers']['version']
-  release version_lock['cuda-drivers']['release']
-  notifies :touch, "file[#{makecache_file}]", :immediately
-end
+selinux_module 'nvidia_docker' do
+  content <<~EOM
+    module nvidia_docker 1.0;
 
-yum_version_lock 'nvidia-docker2' do
-  version version_lock['nvidia-docker2']['version']
-  release version_lock['nvidia-docker2']['release']
-  notifies :touch, "file[#{makecache_file}]", :immediately
-  action :remove
-end
+    require {
+            type xserver_t;
+            type container_runtime_exec_t;
+            class file entrypoint;
+    }
 
-yum_version_lock 'cuda' do
-  version version_lock['cuda']['version']
-  release version_lock['cuda']['release']
-  notifies :touch, "file[#{makecache_file}]", :immediately
+    #============= xserver_t ==============
+    allow xserver_t container_runtime_exec_t:file entrypoint;
+  EOM
+  only_if { node['platform_version'].to_i < 8 }
 end
-
-# Exclude any versions that conflict with what we want
-%w(440 450 455 460 465 470).each do |ver|
-  [
-    "nvidia-driver-branch-#{ver}",
-    "nvidia-driver-branch-#{ver}-cuda",
-    "nvidia-driver-branch-#{ver}-cuda-libs",
-    "nvidia-driver-branch-#{ver}-devel",
-    "nvidia-driver-branch-#{ver}-NvFBCOpenGL",
-    "nvidia-driver-branch-#{ver}-NVML",
-    "nvidia-modprobe-branch-#{ver}",
-    "nvidia-persistenced-branch-#{ver}",
-    "nvidia-xconfig-branch-#{ver}",
-  ].each do |p|
-    yum_version_lock p do
-      version '*'
-      release '*'
-      epoch 3
-      notifies :touch, "file[#{makecache_file}]", :immediately
-    end
-  end
-end
-
-notify_group 'notify yum makecache cuda' do
-  notifies :makecache, 'yum_repository[cuda]', :immediately
-  only_if { ::File.exist?(makecache_file) }
-end
-
-file makecache_file do
-  action :delete
-end
-
-package %w(nvidia-driver-latest-dkms cuda-drivers nvidia-docker2)
