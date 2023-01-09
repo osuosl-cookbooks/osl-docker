7e8b01e4281607d5d7be0f72d64d6e417be67530
Paul Simko <paul@osuosl.org>
2023-01-09 14:00:15 -0800

    Added/updated tests and hashes. Cookstyle fixes

--- spec/unit/recipes/compose_spec.rb
+++ spec/unit/recipes/compose_spec.rb
@@ -1,5 +1,7 @@
 require_relative '../../spec_helper'
 
+version = '2.15.0'
+
 describe 'osl-docker::compose' do
   ALL_PLATFORMS.each do |p|
     context "#{p[:platform]} #{p[:version]}" do
@@ -12,13 +14,31 @@ describe 'osl-docker::compose' do
       it do
         expect(chef_run).to create_remote_file('/usr/local/bin/docker-compose')
           .with(
-            source: 'https://github.com/docker/compose/releases/download/1.18.0/docker-compose-Linux-x86_64',
-            checksum: 'b2f2c3834107f526b1d9cc8d8e0bdd132c6f1495b036a32cbc61b5288d2e2a01',
+            source: "https://github.com/docker/compose/releases/download/#{version}/docker-compose-linux-x86_64",
+            checksum: 'ba481d45be2b137a2a185abd05f61d6d7766dbedfa038f16e4705760767a206e',
             mode: '0755'
           )
       end
       case p
       when CENTOS_7
+        context 'aarch64' do
+          cached(:chef_run) do
+            ChefSpec::SoloRunner.new(p) do |node|
+              node.automatic['kernel']['machine'] = 'aarch64'
+            end.converge(described_recipe)
+          end
+          it do
+            expect(chef_run).to include_recipe 'osl-selinux'
+          end
+          it do
+            expect(chef_run).to create_remote_file('/usr/local/bin/docker-compose')
+              .with(
+                source: "https://github.com/docker/compose/releases/download/#{version}/docker-compose-linux-aarch64",
+                checksum: '14d31297794868520cb2e61b543bb1c821aaa484af22b397904314ae8227f6a2',
+                mode: '0755'
+              )
+          end
+        end
         context 'ppc64le' do
           cached(:chef_run) do
             ChefSpec::SoloRunner.new(p) do |node|
@@ -31,8 +51,8 @@ describe 'osl-docker::compose' do
           it do
             expect(chef_run).to create_remote_file('/usr/local/bin/docker-compose')
               .with(
-                source: 'http://ftp.osuosl.org/pub/osl/openpower/docker-compose/1.18.0/docker-compose-Linux-ppc64le',
-                checksum: '4458de249ca5f776738e44a6af2f869c1186b2d018dd15705045aa01a45e50c5',
+                source: "http://ftp.osuosl.org/pub/osl/openpower/docker-compose/#{version}/docker-compose-linux-ppc64le",
+                checksum: '2b57f69c438fadaa88dc383d8df8b98955a2ab9262c7ca2524102c8199c8efb4',
                 mode: '0755'
               )
           end
