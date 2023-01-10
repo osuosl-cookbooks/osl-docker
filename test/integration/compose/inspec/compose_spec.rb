1c85efc671de2cd91f9cbc7601372ce48e12d992
Paul Simko <paul@osuosl.org>
2023-01-09 17:04:04 -0800

    Updated inspec test

--- test/integration/compose/inspec/compose_spec.rb
+++ test/integration/compose/inspec/compose_spec.rb
@@ -1,4 +1,4 @@
 describe command('/usr/local/bin/docker-compose version') do
   its('exit_status') { should eq 0 }
-  its('stdout') { should match(/docker-compose version 1\.18\.0/) }
+  its('stdout') { should match(/docker-compose version 2\.15\.0/) }
 end
