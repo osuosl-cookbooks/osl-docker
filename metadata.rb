55b7593077485b6665c328ad27a2292dd7ca72f7
Lance Albertson <lance@osuosl.org>
2024-05-03 15:40:08 -0700

    Remove support for Debian 11
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- metadata.rb
+++ metadata.rb
@@ -19,5 +19,4 @@ depends          'osl-selinux'
 
 supports         'almalinux', '~> 8.0'
 supports         'centos', '~> 7.0'
-supports         'debian', '~> 11.0'
 supports         'debian', '~> 12.0'
