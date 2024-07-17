89fbcf02bcff817ac89eb673bba291c8060bf3a8
Manatee <github@osuosl.org>
2024-07-17 23:06:06 +0000

    Automatic minor-level version bump to v4.13.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ osl-docker CHANGELOG
 This file is used to list changes made in each version of the
 osl-docker cookbook.
 
+4.13.0 (2024-07-17)
+-------------------
+- Remove support for CentOS 7
+
 4.12.2 (2024-07-08)
 -------------------
 - Update to latest docker cookbook to fix excon issues
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ chef_version     '>= 16.0'
 issues_url       'https://github.com/osuosl-cookbooks/osl-docker/issues'
 source_url       'https://github.com/osuosl-cookbooks/osl-docker'
 description      'Installs/Configures osl-docker'
-version          '4.12.2'
+version          '4.13.0'
 
 depends          'apt'
 depends          'certificate'
